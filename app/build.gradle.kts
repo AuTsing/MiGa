0379fcefab92b22441d4dd24eb40d968e0cf5c3d
AuTsing <763025696@qq.com>
2025-05-18 01:45:23 +0800

    0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,8 @@ android {
         applicationId = "com.autsing.miga"
         minSdk = 33
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
+        versionCode = 100
+        versionName = "0.1.0"
     }
 
     signingConfigs {
